3ff801f7f42f4b765c37a84b841ae6df666700f2
abhiraj-ku <abhirajabhi312@gmail.com>
2024-06-29 08:26:03 +0000

    my new nada program

--- programs/my_first_program.py
+++ programs/my_first_program.py
@@ -7,6 +7,7 @@ def nada_main():
     my_int2 = SecretInteger(Input(name="my_int2", party=party1))
 
     # write the computation for your program here - use my_int1 and my_int2 as inputs
-    # make sure you change the output below to be your new output
+    # Compute (my_int1^2 + my_int2^2) - (my_int1 * my_int2)
+    result = (my_int1 * my_int1 + my_int2 * my_int2) - (my_int1 * my_int2)
 
     return [Output(my_int1, "my_output", party1)]
